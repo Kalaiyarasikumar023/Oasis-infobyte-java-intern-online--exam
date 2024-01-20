43505524c5e2733405e9104e4708e8cdd2d95f3e
Kalaiyarasikumar023 <153170475+Kalaiyarasikumar023@users.noreply.github.com>
2024-01-20 17:22:10 +0530

    Create result display.java

---
+++ result display.java
@@ -0,0 +1,9 @@
+public class DisplayResult {
+    public static void main(String[] args) {
+        // Assume you have conducted the exam and obtained the score
+        int score = 5; // Change this to the actual score
+
+        // Display result
+        System.out.println("Your final score: " + score + "/" + questions.size());
+    }
+}
